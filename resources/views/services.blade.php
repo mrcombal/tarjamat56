ec932dffadd4f16a7528a3b54b2f22365d5d9233
MrCombal <wessamcombal@gmail.com>
2024-10-09 14:55:24 +0000

    Fixed hero titles in Services and About pages

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-7">
-                    <h1 class="display-5" style="font-weight: 100; margin-bottom: 20px; margin-top: 30px;">Crafting <span class="fw-bold">words</span> ..<br>.. bridging <span class="fw-bold">worlds</span></h1>
+                    <h1 style="font-weight: 100; margin-bottom: 20px; margin-top: 30px;">Crafting <span class="fw-bold">words</span> ..<br>.. bridging <span class="fw-bold">worlds</span></h1>
                     <p class="lead" style="font-size: 16px; line-height: 24px;">From translation to transcription, we connect cultures and deliver precise communication globally. Let’s bring your message to the world.</p>
                 </div>
                 <div class="col-md-5">
